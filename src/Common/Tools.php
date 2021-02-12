d794893ab8cfe7a4892fdb064c6748c715f539cc
paulohirth <paulo@hirth.com.br>
2021-02-12 17:19:59 -0300

    Update Tools.php

--- src/Common/Tools.php
+++ src/Common/Tools.php
@@ -6,7 +6,7 @@ namespace NFePHP\NFSeSJP\Common;
  * Auxiar Tools Class for comunications with NFSe webserver in Ginfes Standard
  *
  * @category  NFePHP
- * @package   NFePHP\NFSeGinfes
+ * @package   NFePHP\NFSeSJP
  * @copyright NFePHP Copyright (c) 2020
  * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
  * @license   https://opensource.org/licenses/MIT MIT
